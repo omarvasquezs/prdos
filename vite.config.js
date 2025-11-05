2eed1265e7599c8022b38f8a7a04e4f0a4ab76da
Omar Vásquez <omarvs91@gmail.com>
2025-11-05 10:37:51 -0500

    Configuring Vite to silence Sass dependency warnings.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: process.env.DDEV_HOSTNAME,
+            protocol: 'wss'
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/login.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true, // Silencia advertencias de dependencias (Bootstrap)
+            },
+        },
+    },
 });
