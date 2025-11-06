5d1e8a6b4da9b3ac8a10f3d685cd613bb7c91c9e
Omar Vásquez <omarvs91@gmail.com>
2025-11-06 10:16:37 -0500

    Configuring CSRF token headers for axios in login module.

--- resources/js/login.js
+++ resources/js/login.js
@@ -2,6 +2,12 @@ import './bootstrap';
 import { createApp } from 'vue';
 import LoginForm from './components/LoginForm.vue';
 
+// Configure CSRF token for axios
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
 const app = createApp(LoginForm);
 
 if (document.getElementById('login')) {
